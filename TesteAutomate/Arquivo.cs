511bcd593dfb9bd7d80830ee7aa95671b107dc91
Osmar <osmargv99@gmail.com>
2018-06-11 22:31:35 -0300

    first commit

---
+++ TesteAutomate/Arquivo.cs
@@ -0,0 +1,49 @@
+﻿using FileHelpers;
+using System;
+
+namespace TesteAutomate
+{
+
+    [FixedLengthRecord(FixedMode.AllowLessChars)]
+
+    public  class Arquivo
+    {
+
+        [FieldFixedLength(2)]
+        [FieldTrim(TrimMode.Both)]
+        public string TipoRegistro { get; set; }
+
+        [FieldFixedLength(8)]
+        [FieldConverter(ConverterKind.Date, "yyyyMMdd")]
+        public DateTime DataPregao { get; set; }
+
+
+        [FieldFixedLength(2)]
+        [FieldTrim(TrimMode.Both)]
+        public string Branco4 { get; set; }
+
+        [FieldFixedLength(12)]
+        [FieldTrim(TrimMode.Both)]
+        public string CodNegociacao { get; set; }
+
+        [FieldFixedLength(33)]
+        public string Branco1 { get; set; }
+
+        [FieldFixedLength(11)]
+        [FieldConverter(ConverterKind.Decimal)]
+        public decimal ValorAbertura { get; set; }
+
+        [FieldFixedLength(41)]
+        public string Branco2 { get; set; }
+
+        [FieldFixedLength(11)]
+        [FieldConverter(ConverterKind.Decimal)]
+        public decimal ValorFechamento { get; set; }
+
+        [FieldFixedLength(125)]
+        public string Branco3 { get; set; }
+
+
+
+    }
+}
---
+++ TesteAutomate/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+using FileHelpers;
+
+namespace TesteAutomate
+
+{
+    class Program
+    {
+
+
+        static ConcurrentDictionary<Guid, Arquivo>  Arquivos = new ConcurrentDictionary<Guid, Arquivo>();
+     
+        static void Main(string[] args)
+        {
+            var diretorioArquivos  = System.Configuration.ConfigurationManager.AppSettings["diretorioArquivos"];
+            DirectoryInfo Dir = new DirectoryInfo(diretorioArquivos);
+
+            FileInfo[] Files = Dir.GetFiles("*.txt", SearchOption.TopDirectoryOnly);
+
+            Parallel.ForEach(Files, FileArquivo => {
+                var novoArquivo = $" {FileArquivo.FullName.Replace(FileArquivo.Extension,"")}_{Guid.NewGuid()}{FileArquivo.Extension}";
+                var linhas = File.ReadAllLines(FileArquivo.FullName).ToList();
+                linhas.RemoveAt(0);
+                linhas.RemoveAt(linhas.Count - 1);
+
+                using (StreamWriter sr = new StreamWriter(novoArquivo)) {
+                    foreach (var lin in linhas)
+                        sr.WriteLine(lin);
+
+                }
+                var engine = new FixedFileEngine<Arquivo>();
+                Arquivo[] result = engine.ReadFile(novoArquivo);
+                foreach (var arq in result)
+                    Arquivos.AddOrUpdate(Guid.NewGuid(), arq, (key, value) => arq);
+                File.Delete(novoArquivo);
+            });
+            var arquivos =   Arquivos.Select(x=>x.Value).OrderBy(x => x.DataPregao).ToList();
+            Console.ReadKey();
+
+        }
+    }
+}
